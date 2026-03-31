cbce49accf1dfd44ce2260ce7ee84b097355fefe
efleuriet-debug <efleuriet@gmail.com>
2026-03-31 16:28:42 +0200

    Add files via upload

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,4 @@
-const API_URL = https://script.google.com/macros/s/AKfycbwICBwJcaGzX78E983604Uj-FbNq28_nOevRjswkHw9SzXXGXOhW5HbENvgrfv0HOrX/exec;
+const API_URL = "https://script.google.com/macros/s/AKfycbwICBwJcaGzX78E983604Uj-FbNq28_nOevRjswkHw9SzXXGXOhW5HbENvgrfv0HOrX/exec";
 
 // Envoi du formulaire
 const form = document.getElementById("dispoForm");
